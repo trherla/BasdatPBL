3116b998d307c66081e2b0d3d23cc2d98fb00935
trherla <tri.h.is.534@gmail.com>
2020-11-09 09:44:00 +0700

    add image on atribut

--- app/src/main/java/com/herla/basdatpbl/SplashScreen.java
+++ app/src/main/java/com/herla/basdatpbl/SplashScreen.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Objects;
 import java.util.Timer;
@@ -11,14 +15,24 @@ import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
 
+//    ImageView imageView1, imageView2;
+//    TextView textView1, textView2;
+//
+//    Animation fade_animation = AnimationUtils.loadAnimation(this, R.anim.fade_animation);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Objects.requireNonNull(getSupportActionBar()).hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+//        imageView1 = findViewById(R.id.logoapp);
+//        imageView2 = findViewById(R.id.logoum);
+//        textView1 = findViewById(R.id.appname);
+//        textView2 = findViewById(R.id.instance);
+//
+//        imageView1.setAnimation(fade_animation);
         //Set timer for splash screen timeout
-        int timeout = 1500; // make the activity visible for 1.5 seconds
+        int timeout = 3000; // make the activity visible for 3 seconds
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
 
